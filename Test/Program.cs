489a44f92b8366fa79a756dfec10f54d121d82a3
WangChunhe <gameobjectw@163.com>
2018-10-30 16:56:57 +0800

    初始化提交

---
+++ Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Test
+{
+    class MainClass
+    {
+        public int  Num = 0;
+
+        public static void Main(string[] args)
+        {
+            MainClass mc = new MainClass();
+            Console.WriteLine(mc.Num);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
