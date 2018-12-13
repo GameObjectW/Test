1fa733750ff7d63f5d47be3911c6281e8b65ba9d
WangChunhe <gameobjectw@163.com>
2018-12-13 20:01:42 +0800

    添加括号匹配

---
+++ Test/IsValid.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test
+{
+    public class IsValid
+    {
+
+        public bool isValid(String s) {
+            if (s=="")
+            {
+                return true;
+            }
+            Stack<Char> stack = new Stack<Char>();
+
+            foreach (char item in s)
+            {
+                if (item=='('||item=='{'||item=='[')
+                {
+                    stack.Push(item);
+                }else if(item == ')'||item == ']'||item=='}') {
+
+                    if (stack.Count==0)
+                    {
+                        return false;
+                    }
+                    char pre = stack.Pop();
+                    if (item==')'&&pre=='('||item==']'&&pre=='['||item=='}'&&pre=='{')
+                    {
+                        continue;
+                    }
+                    return false;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+
+        public static void Main(String[] args)
+        {
+            IsValid isValid = new IsValid();
+            Console.WriteLine("Is Successful : " + isValid.isValid("{}[](){}"));
+        }
+    }
+
+}
---
+++ Test/LongestCommonPrefix.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    public class LongestCommonPrefix
+    {
+        public string LongestCommonPrefixTest(String[] strs) {
+            if (strs == null || strs.Length == 0)
+            {
+                return "";
+            }
+            if (strs.Length == 1)
+            {
+                return strs[0];
+            }
+            foreach (String str in strs)
+            {
+                if (str=="")
+                {
+                    return "";
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            int currentIndex = -1;
+            String[] strArray = strs;
+
+            char maxNumFirstChar = strs[0][0];
+            while (true)
+            {
+                currentIndex++;
+                if (strArray[0].Length == currentIndex)
+                {
+                    return sb.ToString();
+                }
+                maxNumFirstChar = strs[0][currentIndex];
+                for (int i = 0; i < strArray.Length; i++)
+                {
+                    if (strArray[i].Length == currentIndex||strArray[i][currentIndex]!= maxNumFirstChar)
+                    {
+                        return sb.ToString();
+                    }
+                }
+                sb.Append(maxNumFirstChar);
+            }
+        }
+        //public static void Main(String[] args){
+        //    LongestCommonPrefix r = new LongestCommonPrefix();
+        //    Console.WriteLine(r.LongestCommonPrefixTest(new string[] {"flow", "fl", "flabc"}));
+        //}
+    }
+
+}
